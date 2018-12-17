1085c17a4f923661b20119b4f6b599a6d3fe517b
romualdhournon66 <k2001@cegetel.net>
2018-12-17 22:19:02 +0100

    mon index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="FR">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <title>PHP Partie 9 exercice 4</title>
+    </head>
+    <body>
+        <div class="container" id="page-top">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div align="center">
+                        <h1 align="center">PHP Partie 9 exercice 4</h1>
+                        <h3 align="center">Exercice 4</h3>
+                        <p class="text">Afficher le timestamp du jour.</p>
+                        <p class="text">Afficher le timestamp du mardi 2 août 2016 à 15h00.</p>
+                        <p align="center">timestamp actuel : <b><?= time(); ?></b></p>
+                        <p align="center">timestamp du mardi 2 août 2016 à 15h00 : <b>1470142800</b></p>
+                        <a href="http://www.timestamp.fr">Vérifions ensemble ici</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
